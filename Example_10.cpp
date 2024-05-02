8a7bd1d68a14ea52e49b02d842a7a77921af0ca0
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2024-05-02 19:10:13 +0300

    Create Example_10.cpp

---
+++ Example_10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int grade;
+    cout << "Enter your grade: ";
+    cin >> grade;
+    
+    switch(grade){
+        case 90:
+            cout << "You Got A";
+            break;
+        case 80 :
+            cout << "You Got B";
+            break;
+        case 70 :
+            cout << "You Got C";
+            break;
+        case 60 :
+            cout << "You Got D";
+            break;
+    default :
+            cout << "Sorry , You Got F in";
+    }
+}
+/* For Example if we don't add break; 
+if input was 70 the output will be
+you got c you got d
+*/

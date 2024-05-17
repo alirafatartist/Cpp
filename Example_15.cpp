bad73647c6d1f09d930cfde248cdfbccb4097bdd
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2024-05-17 09:14:45 +0300

    Create Example_15.cpp

---
+++ Example_15.cpp
@@ -0,0 +1,12 @@
+// Write program that prints out numbers from O to 10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   for(int i=0; i<=10;i++){
+       cout << i<< "\n";
+   }
+
+    return 0;
+}

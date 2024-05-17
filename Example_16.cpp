5943e7a99e8cad45c5e824b4b07284dd6e5b3e49
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2024-05-17 09:16:35 +0300

    Create Example_16.cpp

---
+++ Example_16.cpp
@@ -0,0 +1,15 @@
+/*
+Example : Writea program that prints out numbers from to
+0 10 in
+descending order
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+   for(int i=10; i>=0;i--){
+       cout << i<< "\n";
+   }
+
+    return 0;
+}

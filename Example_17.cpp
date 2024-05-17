152027e309cfd147f6de1cd141f699a9eed49c68
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2024-05-17 09:18:59 +0300

    Create Example_17.cpp

---
+++ Example_17.cpp
@@ -0,0 +1,26 @@
+/*
+Example : Write program that prints out the even numbers from 2 to 20
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+   for(int i=0; i<=20;i+=2){
+       cout << i << "\n";
+   }
+
+    return 0;
+}
+
+/*
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+*/
